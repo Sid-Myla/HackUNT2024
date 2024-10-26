321743429dbce645eb73439cbd4112c76082753a
Sid-Myla <119138398+Sid-Myla@users.noreply.github.com>
2024-10-26 17:29:41 -0500

    Created router stuff
    stuff

---
+++ hackunt2024/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+
+const Home = () => {
+  return <div>
+    <div className='background'>
+        <div className='left'>
+
+        </div>
+        <div className='right'>
+            <div className='namebox'>
+                <h1>Hey, I'm Sid</h1>
+                <p>Whats up</p>
+            </div>
+        </div>
+    </div>
+  </div>;
+};
+
+export default Home;
